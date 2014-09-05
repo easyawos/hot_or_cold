3bbf2c88f98c96f71c92efb242cbe509ac47686f
Ezekiel <easyawos@ovi.com>
2014-09-05 16:08:40 +0100

    added functionality

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
 
-
 /*alert("its working");*/
+
+
+/*****Reload button********/
+
+$('#new_game').click(function() {
+    location.reload();
+});
+
+
+/*****Computer's selection********/
 
 var $computer = Math.floor(Math.random() * 100);
 
-	console.log($computer);
+console.log($computer);
+
+
+
+/*****get user input********/
 
 $(".submitbtn").click(function(event){
 		event.preventDefault;
 		user_input();
-			
 });
+
+
+
+/*****Cold or Hot Algorithm********/
 
 var previous_num= 0;
 
 var user_input = function () {
 	var user_number = $(".user_input").val();
 
-	if (isNaN(user_number) || user_number>100 || user_number<0) {
+	if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="") {
 		alert("wrong input");
 		
 	}	
@@ -39,9 +55,9 @@ var user_input = function () {
 			 	 		previous_num = user_value; 
 							
 		}
+		
 }
 
 
-/*alert(Number);
-alert(user_value); */
+
 
