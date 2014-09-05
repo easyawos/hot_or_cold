d1c86d2a863b163a4bc35ee922e0866d23a8c162
Ezekiel <easyawos@ovi.com>
2014-09-05 21:58:45 +0100

    progress bar included

--- js/app.js
+++ js/app.js
@@ -35,8 +35,8 @@ var previous_num= 0;
 var user_input = function () {
 	var user_number = $(".user_input").val();
 
-	if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="") {
-		$(".feedback p").html("Please an integer number between 0 and 100");
+	if (isNaN(user_number) || user_number>100 || user_number<=0 || user_number ==="") {
+		$(".feedback p").html("Enter an integer number between 0 and 100");
 	}	
 
 		else {
@@ -55,7 +55,50 @@ var user_input = function () {
 		 	 			}
 		previous_num = user_value; 
 		}
+
+		var deduce = Math.abs($computer - previous_num);
+		var c_bar =  Math.abs(100-deduce);
+		setProgress(c_bar);
 }
+
+
+function setProgress(progress) {           
+    var progressBarWidth =progress*$(".container").width()/ 100;  
+    $(".progressbar").width(progressBarWidth).html(progress + "% ");
+}
+
+
+
+	/*if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="") {
+		
+		$(".feedback p").html("Please an integer number between 0 and 100");
+
+	}	
+
+		else if (user_value === $computer) {
+			$(".feedback p").html("You Win")
+		}
+
+			else {	
+				var Number = $computer - previous_num;
+				var Number2 = $computer - user_number;
+			}
+
+			if (Number > Number2) {
+				$(".feedback p").html("Hotter");
+				previous_num = user_number;
+			}
+
+				else {
+					$(".feedback p").html("Colder");
+					previous_num = user_number;
+				}
+
+
+		}
+
+} */
+
 
 
 
