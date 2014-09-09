af14b127cfd3637fb534d547d767b1292927bc61
Ezekiel <easyawos@ovi.com>
2014-09-09 17:56:09 +0100

    progress bar fixed

--- js/app.js
+++ js/app.js
@@ -1,99 +1,64 @@
 
-/*****Reload button********/
+var zikiguess= {
 
-$('#new_game').click(function() {
-    location.reload();
-});
+	computer: 0,
+	previous_num:0,
 
+	onReady:function(){
+		zikiguess.computer = Math.floor(Math.random() * 100);
+		zikiguess.previous_num= 0;
+		$('#new_game').click(zikiguess.newGame);
+		$(".submitbtn").click(zikiguess.submit_guess);
+	},
+	
+	newGame: function(){
+		location.reload();
+	},
 
-/*****Computer's selection********/
-
-var $computer = Math.floor(Math.random() * 100);
-
-console.log($computer);
-
-
-
-/*****get user input********/
-
-$(".submitbtn").click(function(event){
+	submit_guess: function(){
 		event.preventDefault;
-		user_input();
-});
+		zikiguess.user_input();
+	},
 
+	user_input: function(){
+		console.log(zikiguess.computer);
+		console.log(zikiguess.previous_num);
 
+		var user_number = $(".user_input").val();
+		if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="") {
+			$(".feedback p").html("Enter an integer number between 0 and 100");
+		}	
 
-/*****Cold or Hot Algorithm********/
+			else {
+				var diffOld = Math.abs(zikiguess.computer - zikiguess.previous_num)
+				var diffNew = Math.abs(zikiguess.computer - user_number);
 
-var previous_num= 0;
+					if (user_number == zikiguess.computer) {	
+						$(".feedback p").html("You Win!!!")
+					} 
+						else if (diffOld > diffNew) {
+							$(".feedback p").html("Hotter")
 
-var user_input = function () {
-	var user_number = $(".user_input").val();
-
-	if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="") {
-		$(".feedback p").html("Enter an integer number between 0 and 100");
-	}	
-
-		else {
-			var user_value = user_number;
-			var diffNew = Math.abs($computer - user_value);
-			var diffOld = Math.abs($computer - previous_num)
-
-				if (user_value == $computer) {	
-					$(".feedback p").html("You Win!!!")
-				} 
-					else if (diffOld > diffNew) {
-						$(".feedback p").html("Hotter")
-
-		 	 		}	else {
-		 	 				$(".feedback p").html("Colder")
-		 	 			}
-		previous_num = user_value; 
-		}
-
-		var deduce = Math.abs($computer - previous_num);
-		var c_bar =  Math.abs(100-deduce);
-		setProgress(c_bar);
-}
-
-
-function setProgress(progress) {           
-    var progressBarWidth =progress*$(".container").width()/ 100;  
-    $(".progressbar").width(progressBarWidth).html(progress + "% ");
-}
-
-
-
-	/*if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="") {
-		
-		$(".feedback p").html("Please an integer number between 0 and 100");
-
-	}	
-
-		else if (user_value === $computer) {
-			$(".feedback p").html("You Win")
-		}
-
-			else {	
-				var Number = $computer - previous_num;
-				var Number2 = $computer - user_number;
+			 	 		}	else if (diffOld < diffNew) {
+			 	 				$(".feedback p").html("Colder")
+			 	 			}
+	 	 		zikiguess.previous_num = user_number; 
 			}
 
-			if (Number > Number2) {
-				$(".feedback p").html("Hotter");
-				previous_num = user_number;
+			if (zikiguess.computer>50) {
+				p_bar= parseInt(100-((Math.abs(zikiguess.computer-user_number)/Math.abs(zikiguess.computer-0)) * 100));
 			}
-
-				else {
-					$(".feedback p").html("Colder");
-					previous_num = user_number;
+				else if (zikiguess.computer<50)  {
+					p_bar= parseInt(100-((Math.abs(zikiguess.computer-user_number)/Math.abs(zikiguess.computer-100)) * 100));
 				}
 
+			var progressBarWidth =p_bar*$(".container").width()/ 100;  
+		    $(".progressbar").width(progressBarWidth).html(p_bar + "% ");			
+	},
+	
+};
 
-		}
-
-} */
+$(document).ready(zikiguess.onReady);
 
 
-
-
+// 100-Math.abs(computer-user)
