4ee858d9edc193acb58c31497d7c9fe3eb3bab88
Ezekiel <easyawos@ovi.com>
2014-09-15 07:53:35 +0100

    first time player included

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 var zikiguess= {
-
 	computer: 0,
 	previous_num:0,
 
@@ -9,6 +8,7 @@ var zikiguess= {
 		zikiguess.previous_num= 0;
 		$('#new_game').click(zikiguess.newGame);
 		$(".submitbtn").click(zikiguess.submit_guess);
+		console.log(zikiguess.computer);
 	},
 	
 	newGame: function(){
@@ -21,44 +21,50 @@ var zikiguess= {
 	},
 
 	user_input: function(){
-		var user_number = $(".user_input").val();
-		if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="") {
+		var user_number = $.trim($(".user_input").val());
+		if (isNaN(user_number) || user_number>100 || user_number<0 || user_number ==="" ) {
 			$(".feedback p").html("Enter an integer number between 0 and 100");
+			$(".user_input").val("");
+			var progressBarWidth = 0 * $(".container").width()/ 100;  
+			$(".progressbar").width(progressBarWidth).html(0 + "% ");
 		}	
-
 			else {
 				var diffOld = Math.abs(zikiguess.computer - zikiguess.previous_num)
 				var diffNew = Math.abs(zikiguess.computer - user_number);
 
-					if (user_number == zikiguess.computer) {	
-						$(".feedback p").html("You Win!!!");
-						$(".user_input").val("");
-					} 
-						else if (diffOld > diffNew) {
-							$(".feedback p").html("Hotter");
-							$(".user_input").val("");
-
-			 	 		}	else if (diffOld < diffNew) {
-			 	 				$(".feedback p").html("Colder");
-								$(".user_input").val("");
-			 	 			}
-	 	 		zikiguess.previous_num = user_number; 
-			}
-
-			if (zikiguess.computer>50) {
-				p_bar= parseInt(100-((Math.abs(zikiguess.computer-user_number)/Math.abs(zikiguess.computer-0)) * 100));
-			}
-				else if (zikiguess.computer<50)  {
-					p_bar= parseInt(100-((Math.abs(zikiguess.computer-user_number)/Math.abs(zikiguess.computer-100)) * 100));
+				if (zikiguess.previous_num == 0 && user_number == zikiguess.computer) {
+					$(".feedback p").html("Whao!!! You got it at your first attempt.");
+					$(".user_input").val("");
 				}
-
-			var progressBarWidth =p_bar*$(".container").width()/ 100;  
-		    $(".progressbar").width(progressBarWidth).html(p_bar + "% ");	
-	},
-	
+					else  {
+						$(".feedback p").html("Its only your first guess, keep trying");
+						$(".user_input").val("");
+					}
+					
+				if (zikiguess.previous_num !== 0 && user_number == zikiguess.computer) {	
+					$(".feedback p").html("You Win!!!");
+					$(".user_input").val("");
+				} 
+					else if (zikiguess.previous_num !== 0 && diffOld > diffNew) {
+						$(".feedback p").html("Hotter");
+						$(".user_input").val("");
+					}
+		 	 		else if (zikiguess.previous_num !== 0 && diffOld < diffNew) {
+		 	 			$(".feedback p").html("Colder");
+						$(".user_input").val("");
+		 	 		}
+	 	 		zikiguess.previous_num = user_number; 
+			
+				if (zikiguess.computer>50) {
+					p_bar= parseInt(100-((Math.abs(zikiguess.computer-user_number)/Math.abs(zikiguess.computer-0)) * 100));
+				}
+					else if (zikiguess.computer<50)  {
+						p_bar= parseInt(100-((Math.abs(zikiguess.computer-user_number)/Math.abs(zikiguess.computer-100)) * 100));
+					}
+				var progressBarWidth =p_bar*$(".container").width()/ 100;  
+			    $(".progressbar").width(progressBarWidth).html(p_bar + "% ");	
+			}
+	}
 };
 
 $(document).ready(zikiguess.onReady);
-
-
-// 100-Math.abs(computer-user)
